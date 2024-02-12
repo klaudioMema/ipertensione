dca1514eaa710d0df4e865d7fb270e31171cd925
klaudio <klaudiomema2000@gmail.com>
2024-02-12 23:11:00 +0100

    bug selectpatient

--- src/main/java/it/univr/ipertensione_hope/Controller/doctor/SelectPatient.java
+++ src/main/java/it/univr/ipertensione_hope/Controller/doctor/SelectPatient.java
@@ -45,7 +45,7 @@ public class SelectPatient implements Initializable {
         } else {
             DoctorAppData.getInstance().setSelectedPatient(selectedPatient);
             Functions.alert("Paziente selezionato correttamente", Alert.AlertType.INFORMATION, (ButtonType button) -> {
-                WindowsManager.loadPage(WindowsManager.mainClass.getResource(directory + "DoctorViewDashboard.fxml"), directory + "DoctorViewDashboard.fxml");
+                WindowsManager.loadPage(WindowsManager.mainClass.getResource(directory + "DoctorDashboard.fxml"), directory + "DoctorDashboard.fxml");
             });
         }
     }
