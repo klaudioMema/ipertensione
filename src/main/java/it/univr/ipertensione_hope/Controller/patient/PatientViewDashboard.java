ad08ea61778f2f8990b07c84a687eba85d61a9e0
klaudio <klaudiomema2000@gmail.com>
2024-02-18 22:28:59 +0100

    alert paziente

--- src/main/java/it/univr/ipertensione_hope/Controller/patient/PatientViewDashboard.java
+++ src/main/java/it/univr/ipertensione_hope/Controller/patient/PatientViewDashboard.java
@@ -1,26 +1,42 @@
 package it.univr.ipertensione_hope.Controller.patient;
 
-import it.univr.ipertensione_hope.Controller.LoginPage;
-import it.univr.ipertensione_hope.Model.Paziente;
+import it.univr.ipertensione_hope.Model.BloodPressureData;
 import it.univr.ipertensione_hope.View.WindowsManager;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Button;
 
-import java.io.IOException;
-
-public class PatientViewDashboard{
+public class PatientViewDashboard implements Initializable {
     @FXML
-    public Label usernameLabel;
+    public Label welcomeLabel;
     @FXML
-    public HBox contentArea;
+    private Label pressureDataLabel;
+    @FXML
+    private Button loadPressureDataButton;
 
+    private String directory = "patient/";
 
+    @FXML
+    private void openPressureDataPage() {
+        String path = directory + "ReportBloodPressure.fxml";
+        WindowsManager.loadPage(WindowsManager.mainClass.getResource(path), path);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        welcomeLabel.setText(PatientAppData.getInstance().getLoggedPatient().getNome());
+
+        // Controlla se sono presenti dati di pressione per la data odierna
+        if (BloodPressureData.hasReportedToday(PatientAppData.getInstance().getLoggedPatient())) {
+            pressureDataLabel.setVisible(false);
+            loadPressureDataButton.setVisible(false);
+        } else {
+            pressureDataLabel.setVisible(true);
+            loadPressureDataButton.setVisible(true);
+        }
+    }
 }
